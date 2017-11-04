f9817638bf6c58af8b63a099c7456dfb57fbfbf0
Elijah Prince <elijahprince73@gmail.com>
2017-11-03 22:49:17 -0700

    added Switch for routing

---
+++ src/components/post_new.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+
+class PostNew extends Component {
+  render() {
+    return (
+      <div>
+        Post New
+      </div>
+    );
+  }
+};
+
+export default PostNew;
