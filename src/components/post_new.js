641b4c0cec601309c42cec36b65964f9a97c2605
Elijah Prince <elijahprince73@gmail.com>
2017-11-04 14:31:13 -0700

    Validating Forms

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -3,10 +3,13 @@ import {Field, reduxForm} from 'redux-form';
 
 
 class PostNew extends Component {
-  renderTitleField(field){
+  renderField(field){
     return(
-      <div>
+      <div className='form-group'>
+        <label>{field.label}</label>
         <input
+          className='form-control'
+          type='text'
           {...field.input}
         />
       </div>
@@ -17,14 +20,43 @@ class PostNew extends Component {
     return (
       <form>
         <Field
+          label='Title'
           name='title'
-          component={this.renderTitleField}
+          component={this.renderField}
+        />
+        <Field
+          label='Categories'
+          name='categories'
+          component={this.renderField}
+        />
+        <Field
+          label='Post Content'
+          name='content'
+          component={this.renderField}
         />
       </form>
     );
   }
 };
 
+function validate(values) {
+  const errors = {}
+  // validate inputs from the values object
+  if (!values.title || values.length < 3) {
+    errors.title = 'Enter a title that is at least three characters!'
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories'
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content'
+  }
+  // if errors is empty then the form is okay to submit
+  // if errors has any properties redux forms assumes form is invalid
+  return errors
+}
+
 export default reduxForm({
-  form:'PostsNewForm'
+  form:'PostsNewForm',
+  validate: validate
 })(PostNew);
